788c73426a2766515a1994b4733c55d42ac1d765
Calvein <calvein@github.com>
2011-12-28 16:13:32 +0100

    Navigation

---
+++ public/js/data.js
@@ -0,0 +1,100 @@
+$(function() {
+    // jQuery objects
+    var $breadcrumb = $('.breadcrumb')
+      , $subDirectory = $('#sub_directory ul')
+      , $sortTable = $('#sortTable');
+    
+    var changeState = function(e) {
+        e.preventDefault();
+        var pathname = e.currentTarget.pathname || e.target.location.pathname;
+        history.pushState({}, 'TITRE MARCHE PAS', pathname);
+        changeContent(pathname)
+    }
+
+    $breadcrumb.on('click', 'a', changeState)
+    $subDirectory.on('click', 'a', changeState)
+    window.addEventListener('popstate', changeState);
+      
+    window.changeContent = function(path, first) {
+        // Remove the last /
+        path = path.replace(/\/$/, '')
+        
+        // Flush the content
+        $breadcrumb.find('li:not(:last-child)').remove();
+        $subDirectory.find('li:not(:first-child)').remove();
+        
+        // Show the breadcrumb
+        var text =  '<li{{^url}} class="active"{{/url}}>\
+                        {{#url}}<a href="{{url}}">{{/url}}\
+                            {{label}}\
+                        {{#url}}</a>{{/url}}\
+                        <span class="divider">/</span>'
+          , template = Hogan.compile(text)
+          , routes = path.split('/')
+          , nbRoutes = routes.length
+          , url = ''
+          , render = path.split('/').map(function(route, i) {
+                url += route + '/'
+                return template.render({
+                    // If i + 1 = nbRoutes, it's the current one
+                    url: i + 1 === nbRoutes ? 0 : url,
+                    label: route || 'Home'
+                });
+            }).join('');
+        
+        $breadcrumb.prepend(render)
+        
+        // Show the sub-directories
+        $.get('/tags', { subdirsof: path || '/' }, function(data) {
+            var text =  '<li><a href="{{url}}" title="{{label}}"><i class="iconic arrow-right-alt"></i><span>{{label}}</span></a>'
+              , template = Hogan.compile(text)
+              , render = data.map(function(dir) {
+                    return template.render({
+                        url: dir.label,
+                        label: dir.label.replace(path + '/', '')
+                    });
+                }).join('');
+            
+            $subDirectory.append(render);
+        });
+        
+        // Show the documents
+        $.get('/documents', { tags: path }, function(data) {
+            var text =  '{{#title}}\
+                        <tr>\
+                            <td>{{title}}\
+                            <td>{{created_at}}\
+                            <td>{{size}} ko\
+                            <td>{{mime}}\
+                            <td><input type="checkbox" name="optionsCheckboxes" />\
+                        {{/title}}\
+                        {{^title}}\
+                        <tr><td>T<td>O<td>D<td>O\
+                        {{/title}}'
+              , template = Hogan.compile(text)
+              , nbDocs = data.length
+              , render = nbDocs ?
+                    data.map(function (doc) {
+                        return template.render({
+                            title: doc.title,
+                            created_at: doc.created_at.split('T')[0].split('-').reverse().join('/'),
+                            // Size from o to ko
+                            size: Math.ceil(doc.resource.size / 1024),
+                            mime: doc.resource.mime.split('/')[1]
+                        });
+                    }).join('') :
+                    template.render({ empty: true });
+            
+            $sortTable.find('tbody').html(render).end()
+            // The first time, initialize tablesorter, afterwards, update it
+            if (first) {
+                $sortTable.tablesorter({ sortList: [[1,0]] });
+            } else {
+                $sortTable.trigger('update');
+            }
+        });
+    }
+    
+    // Remove the last / if present
+    changeContent(location.pathname, true)
+});
--- public/js/main.js
+++ public/js/main.js
@@ -1,84 +1,52 @@
 $(function($){
-
-	/* List documents
-	*/
-	$("table#sortTable").tablesorter({ 
-		sortList: [[1,0]] 
-	});
-	
-	/* Twipsy
-	*/
-	$("[rel=twipsy]").twipsy({
-    live: true
-  })
-
-	/* Tagit
-	*/	
-	var sampleTags = ['c++', 'java', 'php', 'coldfusion', 'javascript', 'asp', 'ruby', 'python', 'c', 'scala', 'groovy', 'haskell', 'perl', 'erlang', 'apl', 'cobol', 'go', 'lua'];
-
-	// Remove confirmation
-	$('#removeConfirmationTags').tagit({
-		availableTags: sampleTags,
-		removeConfirmation: true
-	});
-
-	/* Uploader
-	*/	
-	// Declare Elements
-	var $uploader = $('#uploader'),
-			$file = $('#file', $uploader),
-			$rightbar = $('#rightbar'),
-			$overlayRightbar = $('.overlay', $rightbar);
-	
-	$file.on('click', function(e) {
-		if ('webkitNotifications' in window && webkitNotifications.checkPermission() !== 0)
-			webkitNotifications.requestPermission();
-	});
-	
-	$file.on('change drop',	function(e) {
-		// If it's dropped
-		$uploader.removeClass('over');
-		e.stopPropagation();
-		e.preventDefault();
-		
-		var files = this.files || e.originalEvent.dataTransfer.files;
-		console.log(files)
-	
-		// Pour upload, voir : https://raw.github.com/Calvein/Simple-uploader/gh-pages/index.html
-	});
-
-	$uploader.on('dragenter dragleave', function(e) {
-		$(this).toggleClass('over');
-	});
-	
-	$uploader.on('dragover', function(e) {
-		e.stopPropagation();
-		e.preventDefault();
-	});
-	
-	///////////////
-	//   @TODO   //
-	///////////////
-	var subdirsof = '/';
-	$.get('/tags', { subdirsof: subdirsof}, function(data) {
-	    var text =  '<li><a href="{{url}}" title=""><i class="iconic arrow-right-alt"></i><span>{{label}}</span></a></li>',
-            template = Hogan.compile(text),
-            render = data.map(function (dir) {
-          return template.render({url: dir.label, label: dir.label.replace(subdirsof, '') });
-        }).join('');
-        
-	    $('#sub_directory ul').append(render)
-	});
-	
-	// http://guacamole:3000/documents?tags=/
-	/*
-    $.get('/tags', { subdirsof: '/'}, function(data) {
-        console.log(data);
-        var text =  '{{#data}}\
-                        <li><a href="#" title=""><i class="iconic arrow-right-alt"></i><span>{{label}}</span></a></li>\
-                    {{/data}}';
-        $('#sub_directory ul').append(Hogan.compile(text).render({ data: data }))
-    });
+    /* Twipsy
     */
-	
-});	
+    $("[rel=twipsy]").twipsy({
+        live: true
+    })
+
+    /* Tagit
+    */    
+    var sampleTags = ['c++', 'java', 'php', 'coldfusion', 'javascript', 'asp', 'ruby', 'python', 'c', 'scala', 'groovy', 'haskell', 'perl', 'erlang', 'apl', 'cobol', 'go', 'lua'];
+
+    // Remove confirmation
+    $('#removeConfirmationTags').tagit({
+        availableTags: sampleTags,
+        removeConfirmation: true
+    });
+
+    /* Uploader
+    */    
+    // Declare Elements
+    var $uploader = $('#uploader')
+      , $file = $('#file')
+      , $rightbar = $('#rightbar')
+      , $overlayRightbar = $('.overlay', $rightbar);
+    
+    $file.on('click', function(e) {
+        if ('webkitNotifications' in window && webkitNotifications.checkPermission() !== 0)
+            webkitNotifications.requestPermission();
+    });
+    
+    $file.on('change drop', function(e) {
+        // If it's dropped
+        $uploader.removeClass('over');
+        e.stopPropagation();
+        e.preventDefault();
+        
+        var files = this.files || e.originalEvent.dataTransfer.files;
+        console.log(files)
+    
+        // Pour upload, voir : https://raw.github.com/Calvein/Simple-uploader/gh-pages/index.html
+    });
+
+    $uploader.on('dragenter dragleave', function(e) {
+        $(this).toggleClass('over');
+    });
+    
+    $uploader.on('dragover', function(e) {
+        e.stopPropagation();
+        e.preventDefault();
+    });
+    
+});    
